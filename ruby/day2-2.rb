ac275359c54157939df57d0d0569630c177aa026
Mustafa Haddara <mustafaayhaddara@yahoo.com>
2019-12-02 00:25:43 -0500

    2-2 redone the "smart" way

--- ruby/day2-2.rb
+++ ruby/day2-2.rb
@@ -24,13 +24,31 @@ def vm(instructions, noun=12, verb=2)
         end
         ptr += 4
     end
-    return instructions
+    return instructions[0]
 end
 
-def attempt(str_instructions)
-    instructions = str_instructions.map { |n| n.to_i }
+def find_input(str_instructions, target)
+    noun = 0
+    verb = 0
 
-    return vm(instructions, 79, 60)[0]
+    # increment noun
+    result = 0
+    while result < target
+        instructions = str_instructions.map { |n| n.to_i }
+        noun += 1
+        result = vm(instructions, noun, verb)
+    end
+    noun -= 1  # oops we went too far
+
+    # increment verb
+    result = 0
+    while result < target
+        instructions = str_instructions.map { |n| n.to_i }
+        verb += 1
+        result = vm(instructions, noun, verb)
+    end
+
+    return noun*100 + verb
 end
 
 if __FILE__ == $0
@@ -38,7 +56,7 @@ if __FILE__ == $0
     File.open(ARGV[0], "r") do |file_handle|
         file_handle.each_line do |line|
             puts line
-            puts attempt(line.split(","))
+            puts find_input(line.split(","), 19690720)
         end
       end
     puts total
